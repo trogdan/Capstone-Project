33d8a4b3e5772872eabecab968451fa5f7b0cd7c
Trogdan <mr.trogdan@gmail.com>
2016-05-29 11:51:21 -0400

    adding files, refactoring

--- app/src/main/java/com/xanadu/marker/BlogsItemRecyclerViewAdapter.java
+++ app/src/main/java/com/xanadu/marker/BlogsItemRecyclerViewAdapter.java
@@ -29,7 +29,7 @@ public class BlogsItemRecyclerViewAdapter extends RecyclerView.Adapter<BlogsItem
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_blogs_item, parent, false);
+                .inflate(R.layout.fragment_blogs, parent, false);
         return new ViewHolder(view);
     }
 
