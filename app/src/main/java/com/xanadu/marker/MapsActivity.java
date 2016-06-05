d54d21d22776b8b12f210a0cfba8a00c7803a2d5
Trogdan <mr.trogdan@gmail.com>
2016-06-04 21:01:10 -0400

    integrate location api

--- app/src/main/java/com/xanadu/marker/MapsActivity.java
+++ app/src/main/java/com/xanadu/marker/MapsActivity.java
@@ -1,15 +1,27 @@
 package com.xanadu.marker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -23,7 +35,8 @@ import com.xanadu.marker.data.UpdaterService;
 
 public class MapsActivity
         extends AppCompatActivity
-        implements OnMapReadyCallback, LoaderManager.LoaderCallbacks<Cursor>
+        implements OnMapReadyCallback, LoaderManager.LoaderCallbacks<Cursor>,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
 {
 
     @Override
@@ -63,9 +76,26 @@ public class MapsActivity
 
     private static final String TAG = "MapsActivity";
     private static final int LOADER_ID = 1;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
+    private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
     private LatLngBounds mLatestBounds;
+    private Location mLatestLocation;
+
+    @Override
+    protected void onStart() {
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient != null)
+            mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,8 +112,50 @@ public class MapsActivity
         mapFragment.getMapAsync(this);
 
         getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_FINE_LOCATION);
+
+            }
+        }
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this, this, this).addApi(LocationServices.API).build();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
     @Override
     protected void onPause() {
         super.onPause();
@@ -154,4 +226,27 @@ public class MapsActivity
 
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if ( ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mLatestLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(mLatestLocation.getLatitude(), mLatestLocation.getLongitude()), 10));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
